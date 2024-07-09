6427743acc783b28b4ed55daa1f71d0b61ace938
JayRen <monkey407ya@gmail.com>
2024-07-10 00:25:33 +0800

    修改BaseDAO的import問題

--- src/main/java/com/tia102g3/basedao/BaseDAO.java
+++ src/main/java/com/tia102g3/basedao/BaseDAO.java
@@ -1,13 +1,17 @@
 package com.tia102g3.basedao;
 
-import jdbcutils.JDBCUtils;
-
 import java.lang.reflect.Field;
 import java.lang.reflect.ParameterizedType;
 import java.lang.reflect.Type;
-import java.sql.*;
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.ResultSetMetaData;
+import java.sql.SQLException;
 import java.util.ArrayList;
 import java.util.List;
+
+import com.jdbcutils.JDBCUtils;
 
 /**
  * ClassName： BaseDAO
