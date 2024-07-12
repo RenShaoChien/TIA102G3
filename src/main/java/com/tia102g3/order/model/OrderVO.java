ce6b234487419ceaa600651199f58e224c1a47d2
Finn <rec520430@gmail.com>
2024-07-12 19:58:23 +0800

    路徑更正

--- src/main/java/com/tia102g3/order/model/OrderVO.java
+++ src/main/java/com/tia102g3/order/model/OrderVO.java
@@ -6,7 +6,6 @@ import java.sql.Timestamp;
 public class OrderVO implements Serializable{
 	private Integer orderID;
 	private java.sql.Timestamp orderDate ;
-<<<<<<< HEAD
 	private String status;
 	private Integer totalPrice;
 	private Integer memberID;
@@ -66,65 +65,6 @@ public class OrderVO implements Serializable{
 	}
 
 
-=======
-	private String orderStaus;
-	private Integer totalPrice;
-	private Integer memberID;
-	
-	public OrderVO() {
-		super();
-		// TODO Auto-generated constructor stub
-	}
-
-	public OrderVO(Integer orderId, Timestamp orderDate, String orderStaus, Integer totalPrice, Integer memberId) {
-		super();
-		this.orderID = orderId;
-		this.orderDate = orderDate;
-		this.orderStaus = orderStaus;
-		this.totalPrice = totalPrice;
-		this.memberID = memberId;
-	}
-
-	public Integer getOrderId() {
-		return orderID;
-	}
-
-	public void setOrderId(Integer orderId) {
-		this.orderID = orderId;
-	}
-
-	public java.sql.Timestamp getOrderDate() {
-		return orderDate;
-	}
-
-	public void setOrderDate(java.sql.Timestamp orderDate) {
-		this.orderDate = orderDate;
-	}
-
-	public String getOrderStaus() {
-		return orderStaus;
-	}
-
-	public void setOrderStaus(String orderStaus) {
-		this.orderStaus = orderStaus;
-	}
-
-	public Integer getTotalPrice() {
-		return totalPrice;
-	}
-
-	public void setTotalPrice(Integer totalPrice) {
-		this.totalPrice = totalPrice;
-	}
-
-	public Integer getMemberId() {
-		return memberID;
-	}
-
-	public void setMemberId(Integer memberId) {
-		this.memberID = memberId;
-	}
->>>>>>> refs/heads/master
 	
 	
 	
