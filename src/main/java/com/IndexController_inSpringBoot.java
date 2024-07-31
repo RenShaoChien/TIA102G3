88a204d05928b09b3f157404cff54ad33bfea035
Mark Liu(TIA10211) <tzungneng@gmail.com>
2024-07-31 19:46:50 +0800

    Modify application.properties Add Sping test files //---------------------------- 修改application.properties設定檔 加入Spring測試用的檔案

---
+++ src/main/java/com/IndexController_inSpringBoot.java
@@ -0,0 +1,101 @@
+package com;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.RequestParam;
+
+//import com.dept.model.DeptService;
+//import com.dept.model.DeptVO;
+//import com.emp.model.EmpService;
+//import com.emp.model.EmpVO;
+import com.tia102g3.food.model.FoodService;
+import com.tia102g3.food.model.FoodVO;
+import java.util.*;
+
+
+
+//@PropertySource("classpath:application.properties") // 於https://start.spring.io建立Spring Boot專案時, application.properties文件預設已經放在我們的src/main/resources 目錄中，它會被自動檢測到
+@Controller
+public class IndexController_inSpringBoot {
+	
+	// @Autowired (●自動裝配)(Spring ORM 課程)
+	// 目前自動裝配了EmpService --> 供第66使用
+//	@Autowired
+//	EmpService empSvc;
+//	
+//	@Autowired
+//	DeptService deptSvc;
+	
+//	@Autowired
+//	FoodService foodSvc;
+	
+    // inject(注入資料) via application.properties
+//    @Value("${welcome.message}")
+//    private String message;
+	
+//    private List<String> myList = Arrays.asList("Spring Boot Quickstart 官網 : https://start.spring.io", "IDE 開發工具", "直接使用(匯入)官方的 Maven Spring-Boot-demo Project + pom.xml", "直接使用官方現成的 @SpringBootApplication + SpringBootServletInitializer 組態檔", "依賴注入(DI) HikariDataSource (官方建議的連線池)", "Thymeleaf", "Java WebApp (<font color=red>快速完成 Spring Boot Web MVC</font>)");
+    @GetMapping("/")
+    public String index(Model model) {
+//    	model.addAttribute("message", message);
+//        model.addAttribute("myList", myList);
+        return "index"; //view
+    }
+    
+    // http://......../hello?name=peter1
+    @GetMapping("/hello")
+    public String indexWithParam(
+            @RequestParam(name = "name", required = false, defaultValue = "") String name, Model model) {
+        model.addAttribute("message", name);
+        return "index"; //view
+    }
+    
+  
+    //=========== 以下第63~75行是提供給 /src/main/resources/templates/back-end/emp/select_page.html 與 listAllEmp.html 要使用的資料 ===================   
+    @GetMapping("/emp/select_page")
+	public String select_page(Model model) {
+		return "back-end/emp/select_page";
+	}
+    
+    @GetMapping("/emp/listAllEmp")
+	public String listAllEmp(Model model) {
+		return "back-end/emp/listAllEmp";
+	}
+    
+//    @ModelAttribute("empListData")  // for select_page.html 第97 109行用 // for listAllEmp.html 第85行用
+//	protected List<EmpVO> referenceListData(Model model) {
+//		
+//    	List<EmpVO> list = empSvc.getAll();
+//		return list;
+//	}
+//    
+//	@ModelAttribute("deptListData") // for select_page.html 第135行用
+//	protected List<DeptVO> referenceListData_Dept(Model model) {
+//		model.addAttribute("deptVO", new DeptVO()); // for select_page.html 第133行用
+//		List<DeptVO> list = deptSvc.getAll();
+//		return list;
+//	}
+	
+	//================ food test ====================
+//	@GetMapping("/food/select_page")
+//	public String select_page_food(Model model) {
+//		return "back-end/food/select_page";
+//	}
+//	
+//	@GetMapping("/food/listAllFood")
+//	public String listAllFood(Model model) {
+//		return "back-end/food/listAllFood";
+//	}
+//	
+//	@ModelAttribute("foodListData")  // for select_page.html 第97 109行用 // for listAllEmp.html 第85行用
+//	protected List<FoodVO> referenceListData_Food(Model model) {
+//		
+//    	List<FoodVO> list = foodSvc.getAll();
+//		return list;
+//	}
+	
+	
+}
---
+++ src/main/java/com/Test_Application_CommandLineRunner.java
@@ -0,0 +1,158 @@
+package com;
+
+import java.util.List;
+
+import org.hibernate.SessionFactory;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+//import com.dept.model.DeptVO;
+//import com.emp.model.EmpRepository;
+//import com.emp.model.EmpVO;
+import com.tia102g3.food.model.FoodVO;
+//import com.tia102g3.food.model.FoodRepository;
+
+@SpringBootApplication
+public class Test_Application_CommandLineRunner implements CommandLineRunner {
+    
+//	@Autowired
+//	EmpRepository repository ;
+//	
+//	@Autowired
+//	FoodRepository foodRepository;
+	
+	@Autowired
+    private SessionFactory sessionFactory;
+	
+	public static void main(String[] args) {
+        SpringApplication.run(Test_Application_CommandLineRunner.class);
+    }
+
+    @Override
+    public void run(String...args) throws Exception {
+
+    	//● 新增
+//		DeptVO deptVO = new DeptVO(); // 部門POJO
+//		deptVO.setDeptno(1);
+
+//		EmpVO empVO1 = new EmpVO();
+//		empVO1.setEname("吳永志1");
+//		empVO1.setJob("MANAGER");
+//		empVO1.setHiredate(java.sql.Date.valueOf("2005-01-01"));
+//		empVO1.setSal(new Double(50000));
+//		empVO1.setComm(new Double(500));
+//		empVO1.setDeptVO(deptVO);
+//		repository.save(empVO1);
+
+		//● 修改
+//		EmpVO empVO2 = new EmpVO();
+//		empVO2.setEmpno(7001);
+//		empVO2.setEname("吳永志2");
+//		empVO2.setJob("MANAGER2");
+//		empVO2.setHiredate(java.sql.Date.valueOf("2002-01-01"));
+//		empVO2.setSal(new Double(20000));
+//		empVO2.setComm(new Double(200));
+//		empVO2.setDeptVO(deptVO);
+//		repository.save(empVO2);
+		
+		//● 刪除   //●●● --> EmployeeRepository.java 內自訂的刪除方法
+//		repository.deleteByEmpno(7014);
+		
+		//● 刪除   //XXX --> Repository內建的刪除方法目前無法使用，因為有@ManyToOne
+//		System.out.println("--------------------------------");
+//		repository.deleteById(7001);      
+//		System.out.println("--------------------------------");
+
+    	//● 查詢-findByPrimaryKey (多方emp2.hbm.xml必須設為lazy="false")(優!)
+//    	Optional<EmpVO> optional = repository.findById(7001);
+//		EmpVO empVO3 = optional.get();
+//		System.out.print(empVO3.getEmpno() + ",");
+//		System.out.print(empVO3.getEname() + ",");
+//		System.out.print(empVO3.getJob() + ",");
+//		System.out.print(empVO3.getHiredate() + ",");
+//		System.out.print(empVO3.getSal() + ",");
+//		System.out.print(empVO3.getComm() + ",");
+//		// 注意以下三行的寫法 (優!)
+//		System.out.print(empVO3.getDeptVO().getDeptno() + ",");
+//		System.out.print(empVO3.getDeptVO().getDname() + ",");
+//		System.out.print(empVO3.getDeptVO().getLoc());
+//		System.out.println("\n---------------------");
+      
+    	
+		//● 查詢-getAll (多方emp2.hbm.xml必須設為lazy="false")(優!)
+//    	List<EmpVO> list = repository.findAll();
+//		for (EmpVO aEmp : list) {
+//			System.out.print(aEmp.getEmpno() + ",");
+//			System.out.print(aEmp.getEname() + ",");
+//			System.out.print(aEmp.getJob() + ",");
+//			System.out.print(aEmp.getHiredate() + ",");
+//			System.out.print(aEmp.getSal() + ",");
+//			System.out.print(aEmp.getComm() + ",");
+//			// 注意以下三行的寫法 (優!)
+//			System.out.print(aEmp.getDeptVO().getDeptno() + ",");
+//			System.out.print(aEmp.getDeptVO().getDname() + ",");
+//			System.out.print(aEmp.getDeptVO().getLoc());
+//			System.out.println();
+//		}
+
+
+		//● 複合查詢-getAll(map) 配合 req.getParameterMap()方法 回傳 java.util.Map<java.lang.String,java.lang.String[]> 之測試
+//		Map<String, String[]> map = new TreeMap<String, String[]>();
+//		map.put("empno", new String[] { "7001" });
+//		map.put("ename", new String[] { "KING" });
+//		map.put("job", new String[] { "PRESIDENT" });
+//		map.put("hiredate", new String[] { "1981-11-17" });
+//		map.put("sal", new String[] { "5000.5" });
+//		map.put("comm", new String[] { "0.0" });
+//		map.put("deptno", new String[] { "1" });
+//		
+//		List<EmpVO> list2 = HibernateUtil_CompositeQuery_Emp3.getAllC(map,sessionFactory.openSession());
+//		for (EmpVO aEmp : list2) {
+//			System.out.print(aEmp.getEmpno() + ",");
+//			System.out.print(aEmp.getEname() + ",");
+//			System.out.print(aEmp.getJob() + ",");
+//			System.out.print(aEmp.getHiredate() + ",");
+//			System.out.print(aEmp.getSal() + ",");
+//			System.out.print(aEmp.getComm() + ",");
+//			// 注意以下三行的寫法 (優!)
+//			System.out.print(aEmp.getDeptVO().getDeptno() + ",");
+//			System.out.print(aEmp.getDeptVO().getDname() + ",");
+//			System.out.print(aEmp.getDeptVO().getLoc());
+//			System.out.println();
+//		}
+		
+
+		//● (自訂)條件查詢
+//		List<EmpVO> list3 = repository.findByOthers(7001,"%K%",java.sql.Date.valueOf("1981-11-17"));
+//		if(!list3.isEmpty()) {
+//			for (EmpVO aEmp : list3) {
+//				System.out.print(aEmp.getEmpno() + ",");
+//				System.out.print(aEmp.getEname() + ",");
+//				System.out.print(aEmp.getJob() + ",");
+//				System.out.print(aEmp.getHiredate() + ",");
+//				System.out.print(aEmp.getSal() + ",");
+//				System.out.print(aEmp.getComm() + ",");
+//				// 注意以下三行的寫法 (優!)
+//				System.out.print(aEmp.getDeptVO().getDeptno() + ",");
+//				System.out.print(aEmp.getDeptVO().getDname() + ",");
+//				System.out.print(aEmp.getDeptVO().getLoc());
+//				System.out.println();
+//			}
+//		} else System.out.print("查無資料\n");
+//		System.out.println("--------------------------------");
+		
+		//● 查詢-getAll (多方emp2.hbm.xml必須設為lazy="false")(優!)
+//    	List<FoodVO> listfood = foodRepository.findAll();
+//		for (FoodVO foodVO : listfood) {
+//			
+//			System.out.print(foodVO.getFoodNumber() + ",");
+//			System.out.print(foodVO.getFoodTypeNumber() + ",");
+//			System.out.print(foodVO.getFoodName() + ",");
+//			System.out.print(foodVO.getFoodCalories() + ",");
+//			System.out.println();
+//		}
+		
+    }
+}
