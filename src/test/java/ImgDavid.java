71a2442f45f43212f30ce9d78008f4a6f090ad09
JayRen <monkey407ya@gmail.com>
2024-08-15 14:55:27 +0800

    jayren_push 1.驗收前整理

---
+++ src/test/java/ImgDavid.java
@@ -0,0 +1,11 @@
+/**
+ * ClassName： ImgDavid
+ * package：PACKAGE_NAME
+ * Description：
+ *
+ * @Author 任少騫
+ * @Create 2024/8/15 下午2:54
+ * @Version 1.0
+ */
+public class ImgDavid {
+}
---
+++ src/test/java/ImgJayren.java
@@ -0,0 +1,11 @@
+/**
+ * ClassName： ImgJayren
+ * package：PACKAGE_NAME
+ * Description：
+ *
+ * @Author 任少騫
+ * @Create 2024/8/15 下午2:54
+ * @Version 1.0
+ */
+public class ImgJayren {
+}
