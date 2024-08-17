3f152556a46eeade2d39b6ed62304436c56a0979
Mark Liu(TIA10211) <tzungneng@gmail.com>
2024-08-17 22:30:08 +0800

    Modify the shopping functionality
    Add order check.
    
    Modify front-end pages.

--- src/main/java/com/controllers/MenuPageController.java
+++ src/main/java/com/controllers/MenuPageController.java
@@ -4,12 +4,12 @@ import java.util.ArrayList;
 import java.util.List;
 
 import javax.servlet.http.HttpServletRequest;
-import javax.servlet.http.HttpSession;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 
 import com.controllers.mark.ProductInfo;
 import com.controllers.mark.RedisService;
@@ -17,6 +17,8 @@ import com.tia102g3.adminlogin.service.AdminLoginService;
 import com.tia102g3.food.model.FoodService;
 import com.tia102g3.healthstatus.model.HealthStatusVO;
 import com.tia102g3.likefood.model.LikeFoodVO;
+import com.tia102g3.member.model.Member;
+import com.tia102g3.member.model.MemberRepository;
 import com.tia102g3.menu.model.MenuService;
 import com.tia102g3.menu.model.MenuVO;
 
@@ -34,6 +36,9 @@ public class MenuPageController {
     
     @Autowired
     private RedisService redisService;
+    
+    @Autowired
+    private MemberRepository memberSvc;
 
     @GetMapping("/menu")
     public String menu(Model model, HttpServletRequest request) {
@@ -55,7 +60,7 @@ public class MenuPageController {
     }
     
     
-    // test shopping cart
+    //========== test shopping cart ==========
     @GetMapping("/shoppingcart")
     public String shoppingcart(Model model) {
         List<ProductInfo> productList = redisService.getProductList("1234");
@@ -71,5 +76,35 @@ public class MenuPageController {
         
         return "product/shopping_cart";
     }
+    
+//    @GetMapping("/credit_card")
+//    public String creditCard(Model model) {  
+//        
+//        return "product/credit_card";
+//    }
+    
+    @PostMapping("/orderCheck")
+    public String orderCheck(Model model) {
+        List<ProductInfo> productList = redisService.getProductList("1234");
+        model.addAttribute("productList", productList);
+        Member member = memberSvc.getById(1);
+        model.addAttribute("member",member);
+        
+        return "product/order_checking";
+    }
+    
+//    @GetMapping("/delivery")
+//    public String delivery(Model model) {        
+//        
+//        return "product/delivery";
+//    }
+    
+    @GetMapping("/orderFinish")
+    public String delivery(Model model) {        
+        redisService.deleteProductList("1234");
+        return "product/order_finish";
+    }
+    
+    
     
 }
